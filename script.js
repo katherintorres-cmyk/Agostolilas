5beaa746fefd96ff348cd69d50a0abb72b8f1a42
katherintorres-cmyk <katherin.torres@escola.pr.gov.br>
2026-08-20 15:16:30 +0000

    20/08/2026

---
+++ script.js
@@ -0,0 +1,27 @@
+const botaolike = document.querySelector(".like");
+const botaodeslike = document.querySelector(".deslike");
+
+botaolike.addEventListener("click",clicklike);
+botaodeslike.addEventListener("click",clickDeslike);
+
+
+function clicklike () {
+
+    console.log("fui clicado");
+    let texto1=botaolike.querySelector("span");
+    texto1.textContent++;
+
+   
+}
+
+function clickDeslike () {
+
+
+      console.log ("ui clicado");
+      let texto2=botaodeslike.querySelector("span");
+      texto2.textContent++;
+
+
+
+
+}
